e0aed7f8646c7522987922116746cf40a48c02e8
Okan Kırık <okankirik@gmail.com>
2024-12-09 00:03:12 +0300

    Frontend completed.

---
+++ Controllers/WoodenStandController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PirasyonelWebsite.Controllers
+{
+    public class WoodenStandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
