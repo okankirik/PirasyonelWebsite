bab63b16259c60dcb78b0ddcd7a1b105713a9deb
Okan Kırık <okankirik@gmail.com>
2025-01-02 22:52:00 +0300

    [Admin] System Stand create added.

--- Controllers/SystemStandController.cs
+++ Controllers/SystemStandController.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using PirasyonelWebsite.DAL.Context;
 using PirasyonelWebsite.DAL.Entities;
+using PirasyonelWebsite.Utils;
 
 namespace PirasyonelWebsite.Controllers;
 
@@ -32,16 +33,32 @@ public class SystemStandController : Controller
     }
 
     [HttpGet]
-    public IActionResult CreateAdminSystemStand()
+    public async Task<IActionResult> CreateAdminSystemStand()
     {
         return View();
     }
 
     [HttpPost]
-    public IActionResult CreateAdminSystemStand(SystemStand systemStand)
+    public async Task<IActionResult> CreateAdminSystemStand(SystemStand systemStand, IFormFile Image1,  IFormFile Image2, IFormFile? Image3, IFormFile? Image4, IFormFile? Image5)
     {
-        _context.systemStands.Add(systemStand);
-        _context.SaveChanges();
-        return RedirectToAction("AdminSystemStandList");
+        if (ModelState.IsValid)
+        {
+            try
+            {
+                systemStand.Image1 = await FileHelper.FileLoaderAsync(Image1, "/images/SystemStand/");
+                systemStand.Image2 = await FileHelper.FileLoaderAsync(Image2, "/images/SystemStand/");
+                systemStand.Image3 = await FileHelper.FileLoaderAsync(Image3, "/images/SystemStand/");
+                systemStand.Image4 = await FileHelper.FileLoaderAsync(Image4, "/images/SystemStand/");
+                systemStand.Image5 = await FileHelper.FileLoaderAsync(Image5, "/images/SystemStand/");
+                await _context.systemStands.AddAsync(systemStand);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("AdminSystemStandList");
+            }
+            catch
+            {
+                ModelState.AddModelError("", "Stand ekleme başarısız. Lütfen tekrar deneyin.");
+            }
+        }
+        return View(systemStand);
     }
 }
--- DAL/Entities/WoodenStand.cs
+++ DAL/Entities/WoodenStand.cs
@@ -1,14 +1,23 @@
-﻿namespace PirasyonelWebsite.DAL.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PirasyonelWebsite.DAL.Entities;
 
 public class WoodenStand
 {
     public int Id { get; set; }
+    [Display(Name = "Stand Adı")]
     public string StandName { get; set; }
+    [Display(Name = "Stand Kategorisi")]
     public string StandCategoryName { get; set; }
+    [Display(Name = "Resim 1")]
     public string Image1 { get; set; }
+    [Display(Name = "Resim 2")]
     public string Image2 { get; set; }
+    [Display(Name = "Resim 3")]
     public string? Image3 { get; set; }
+    [Display(Name = "Resim 4")]
     public string? Image4 { get; set; }
+    [Display(Name = "Resim 5")]
     public string? Image5 { get; set; }
 
 }
